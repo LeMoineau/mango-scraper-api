558f6e01e3fc4e59436447796016bdb63d6ae5c2
Pierrot <faberbastian@gmail.com>
2024-03-04 11:48:25 +0000

    logging error

--- dist/src/routes/latest-chapters-routes.js
+++ dist/src/routes/latest-chapters-routes.js
@@ -64,7 +64,7 @@ router.get("/", (req, res) =>
         console.error(err.request.rawHeaders);
         console.error(err.request._headers);
         console.error(err.request.kHeaders);
-        res.status(500).send(err.request);
+        res.status(500).send(err.request + "");
       }
     } catch (err) {
       console.error(err);
